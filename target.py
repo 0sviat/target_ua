4aff060204e9aeed9cb5abaa9b17d6a923f92acd
Sviat <sviatoslav.lushnei@ucu.edu.ua>
2021-10-26 01:46:39 +0300

    rule_correctness

--- target.py
+++ target.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 import random
 def generate_grid() -> List[List[str]]:
     """
@@ -8,25 +7,122 @@ def generate_grid() -> List[List[str]]:
     e.g. [['I', 'G', 'E'], ['P', 'I', 'S'], ['W', 'M', 'G']]
     """
     letters =[]
-    for i in range(3):
-        letters.append([])
-        for j in range(3):
-            letters[-1].append(chr(random.randint(65,90)))
-    print(letters)
+    letter_number = 0
+    while letter_number<9:
+        if letter_number%3 == 0:
+            letters.append([])
+        letters[-1].append(chr(random.randint(65,90)))
+        letter_number+=1
+    return letters
 
-generate_grid()
+# print(generate_grid())
 
+def list_of_letter_tuples(word):
+    """
+    str -> list(tuple(str,int))
+    Create list of tuples with letter of the word  and its number of occurrences
+    Letter will be displayed in alphabetic order in lower registor
+    If there are other symbols in a word(1,2,%,&...) they will be omitted
+    >>> list_of_letter_tuples('potato')
+    [('a', 1), ('o', 2), ('p', 1), ('t', 2)]
+    [('a', 1), ('e', 1), ('l', 1), ('p', 2)]
+    >>> list_of_letters_tuples('PyThOn')
+    [('h', 1), ('n', 1), ('o', 1), ('p', 1), ('t', 1), ('y', 1)]
+    >>> list_of_letter_tuples('ERROR')
+    [('e', 1), ('o', 1), ('r', 3)]
+    """
+    letters_list = []
+    word= word.lower()
+    for ord_index in range (97,123):
+        # check by letter ACS if it is in a word
+        if chr(ord_index) in word:
+            letters_list.append(((chr(ord_index), word.count(chr(ord_index)))))
+    return letters_list
+# print(list_of_letter_tuples('azAZ'))
 
-
-
-
-def get_words(f: str, letters: List[str]) -> List[str]:
+def get_words(file_with_words: str, letters: List[str]):
     """
     Reads the file f. Checks the words with rules and returns a list of words.
     """
-    pass
+    # create list of tuples with letters and its number of occurrences
+    grid_letters = list_of_letter_tuples(''.join(letters[0] + letters[1] + letters[2]))
+    main_letter = letters[1][1]
+    words_from_dictionary = []
+    # open file with words and read it line by line
+    with open (file_with_words, mode = 'r', encoding='utf-8') as dictionary:
+        for word in dictionary:
+            # read new word from the file
+            current_word = word.read()
+            # if word is too short or too long do not add it to the list, continue loop
+            if len(current_word) < 4 or len(current_word) > 9 or main_letter not in current_word:
+                continue
+
+            # create list[tuple] with letters of the word from dict and its number of occurrences
+            current_word_letters = list_of_letter_tuples(current_word)
+
+            # check if it's possible to fold current word from dictionary by letters in grid
+            startig_index_in_grid_letters, index_in_word = 0, 0
+            correct_word = True  #default correct_word value
+            while index_in_word < len(current_word_letters):
+                letter_in = False # default letter_in value
+                # check if current letter from a word is in grid
+                for i in range(startig_index_in_grid_letters, len(grid_letters)):
+                    if  current_word_letters[index_in_word][0] == grid_letters[i][0]:
+                        # if it is in grid then check if its number of occurrences
+                        # in the word is not more than in the grid and exit the loop
+                        if current_word_letters[index_in_word][1] <= grid_letters[i][1]:
+                            letter_in = True
+                            startig_index_in_grid_letters = i+1
+                        break
+                # if current letter is not in grid exit the loop, do not add the word to list
+                if not letter_in:
+                    correct_word = False
+                    break
+                index_in_word += 1   # change letter in word_letter_list to next one
+            if correct_word:
+                words_from_dictionary.append(current_word)
+
+    return words_from_dictionary
 
 
+def rule_correctness_word(word, grid_letters):
+    """
+    (str, list) -> bool
+    Check if word fits game roles.
+    Word lenght in range [4,9].
+    Word includes central letter
+    All letters in the word are from grid board and itd
+    number of occurrences in words is not more than in the grid board
+
+    """
+    word = ''
+    central_letter = grid_letters[1][1]
+    # if word is too short or too long do not add it to the list, continue loop
+    if len(word) < 4 or len(word) > 9 or central_letter not in word:
+        return False
+
+    # create list[tuple] with letters of the word from dict and its number of occurrences
+    current_word_letters = list_of_letter_tuples(word)
+
+    # check if it's possible to fold word from dictionary by letters in grid
+    startig_index_in_grid_letters, index_in_word = 0, 0
+    correct_word = True  #default correct_word value
+    while index_in_word < len(current_word_letters):
+        letter_in = False # default letter_in value
+        # check if current letter from a word is in grid
+        for i in range(startig_index_in_grid_letters, len(grid_letters)):
+            if  current_word_letters[index_in_word][0] == grid_letters[i][0]:
+                # if it is in grid then check if its number of occurrences
+                # in the word is not more than in the grid and exit the loop
+                if current_word_letters[index_in_word][1] <= grid_letters[i][1]:
+                    letter_in = True
+                    startig_index_in_grid_letters = i+1
+                break
+        # if current letter is not in grid exit the loop, do not add the word to list
+        if not letter_in:
+            correct_word = False
+            break
+        index_in_word += 1   # change letter in word_letter_list to next one
 
 def get_user_words() -> List[str]:
     """
@@ -42,16 +138,18 @@ def get_user_words() -> List[str]:
     return list_of_player_worlds
 
 
-
-
 def get_pure_user_words(user_words: List[str], letters: List[str], words_from_dict: List[str]) -> List[str]:
     """
     (list, list, list) -> list
-
     Checks user words with the rules and returns list of those words
     that are not in dictionary.
     """
-    pass
+    wrong_words = []
+    main_letter = letters[1][1]
+    for word in user_words:
+        if not rule_correctness_word(word,letters):
+            continue
+        word = list_of_letter_tuples(user_words)
 
 
 def results():
